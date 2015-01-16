740148c963e3376de879fcd80b6b047d7c3ff7d2
Stef van Dijk <stef@vandijkstef.nl>
2015-01-17 00:20:13 +0100

    Completed image rendering on single and gallery
    Generated one layout chunk for both views

--- routers/gallery.js
+++ routers/gallery.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// Render the full gallery
 router.get('/', function(req, res){
 	req.getConnection(function(err, connection){
 		var sql = 'SELECT photos.*, users.name AS username FROM photos LEFT JOIN users ON photos.user_id = users.id';
@@ -20,11 +21,12 @@ router.get('/', function(req, res){
 	});
 });
 
+// Redirect back to gallery if there is no image ID
 router.get('/image', function(req, res){
-	// Redirect back to gallery if there is no image ID
 	res.redirect('/gallery');
 });
 
+// Render the single image page
 router.get('/image/:id', function(req,res){
 	var imageId = req.param("id");
 	console.log(imageId);
@@ -36,10 +38,16 @@ router.get('/image/:id', function(req,res){
 				var message = "Database Error" + err;
 				var data = { req: req, message: message }
 				res.render('gallery/image.ejs', data);
-			} else {
-				console.log(image[0]);
+			} else if(image[0] == undefined) {
+				// 404 is image does not exist
+					// To add : Really check if image is available on Filestorage
+				var message = "404: Image is not available";
+				var data = { req: req, message: message }
+				res.render('gallery/image.ejs', data);
+			} else{
+				console.log(image);
 				var message = null;
-				var data = { req: req, message: message, image: image[0] }
+				var data = { req: req, message: message, images: image }
 				res.render('gallery/image.ejs', data);
 			}
 		});
