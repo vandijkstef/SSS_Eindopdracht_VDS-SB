db5117c87b22300fbe6113ebadcf748f72ca1922
Stef van Dijk <stef@vandijkstef.nl>
2015-01-16 23:46:02 +0100

    Added username to template

--- routers/gallery.js
+++ routers/gallery.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 router.get('/', function(req, res){
 	req.getConnection(function(err, connection){
-			var sql = 'SELECT * FROM photos';
+			var sql = 'SELECT photos.*, users.name AS username FROM photos LEFT JOIN users ON photos.user_id = users.id';
 			console.log(sql);
 		    connection.query(sql, function(err, gallery){
 		      	if(err) {
-		      		console.log("SQL Error");
-		      		return;
+		      		var message = "Database Error" + err;
+					var data = { req: req, message: message }
+					res.render('gallery/index.ejs', data);
 		      	} else {
 		      		console.log(gallery);
-		      		var message = "SQL Worked";
+		      		var message = null;
 		      		var images = gallery;
 					var data = { req: req, message: message, images: images }
 					res.render('gallery/index.ejs', data);
