f32d2ae8069768461948b4d7e290b2ca1646b8b5
Stef van Dijk <stef@vandijkstef.nl>
2015-01-26 18:42:28 +0100

    Extra form validation + commenting

--- routers/gallery.js
+++ routers/gallery.js
@@ -20,13 +20,14 @@ router.get('/image/:id', function(req, res){
 	displayImg(data, imageId);
 });
 
+// Process the comment form
 router.post('/image/:id', function(req, res){
 	req.getConnection(function(err, connection){
 		// Get all vars
 		var imageId = req.param("id");
 		var comment = req.body.comment;
 		var username = req.body.username;
-		var is_user = 0;
+		var is_user = 0; // To be done - Check if user is a logged in user
 		// Validate
 		if (comment == undefined || comment.length < 5) {
 			var message = "Comment too short";
@@ -34,21 +35,34 @@ router.post('/image/:id', function(req, res){
 			displayImg(data, imageId);
 			return
 		}
+		// Set username to anonymous
 		if (username == undefined) {
 			username = 'anonymous';
 		}
-		var sql = 'INSERT INTO comments (photo_id, created_at, comment, username, is_user) VALUES (' + imageId + ',"' + new Date().toISOString().slice(0, 19).replace('T', ' ') + '","' + comment + '", "' + username + '",' + is_user + ')';
-		connection.query(sql, function(err, comments){
-			if(err) {
-	      		var message = "Database Error" + err;
+		// Check if the comment is not double
+		var sql = 'SELECT * FROM comments WHERE comment = "' + comment + '" AND username = "' + username + '" AND photo_id = "' + imageId + '"';
+		connection.query(sql, function(err, doublecomment){
+			if (doublecomment.length >= 1) {
+				var message = "Comment exsists - A bit trigger happy?";
 				var data = { req: req, res: res, message: message }
 				displayImg(data, imageId);
-	      	} else {
-	      		var message = "Comment Inserted";
-				var data = { req: req, res: res, message: message }
-				displayImg(data, imageId);
-	      	}
-		});
+				return
+			} else {	
+				// Insert the comment
+				var sql2 = 'INSERT INTO comments (photo_id, created_at, comment, username, is_user) VALUES (' + imageId + ',"' + new Date().toISOString().slice(0, 19).replace('T', ' ') + '","' + comment + '", "' + username + '",' + is_user + ')';
+				connection.query(sql2, function(err, comments){
+					if(err) {
+			      		var message = "Database Error: " + err;
+						var data = { req: req, res: res, message: message }
+						displayImg(data, imageId);
+			      	} else {
+			      		var message = "Comment Inserted";
+						var data = { req: req, res: res, message: message }
+						displayImg(data, imageId);
+			      	}
+				});	
+			}
+		})
 	});
 });
 
@@ -100,7 +114,6 @@ var displayImg = function (data, imageId) {
 								console.log(err);
 							} else {
 								data.comments = commentsarray;
-								console.log(data.comments);
 								data.res.render('gallery/index.ejs', data); // Render - Waited on SQL
 							}
 						})
